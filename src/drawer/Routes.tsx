d3ffe11a9829108770bebb92d3b911ae3120b024
Yakub <jak.miskech@gmail.com>
2021-04-17 15:59:05 +0200

    Add icons for drawer items

--- src/drawer/Routes.tsx
+++ src/drawer/Routes.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import { RootParamList } from '@constants/navigation';
+import { theme } from '@constants/theme';
 
 import DashboardStack from '../screens/dashboard';
 import MeasurementsStack from '../screens/measurements';
@@ -11,10 +13,34 @@ const Drawer = createDrawerNavigator<RootParamList>();
 
 export default function DrawerRoutes() {
 	return (
-		<Drawer.Navigator initialRouteName="Dashboard">
-			<Drawer.Screen name="Dashboard" component={DashboardStack} />
-			<Drawer.Screen name="Measurements" component={MeasurementsStack} />
-			<Drawer.Screen name="Challenges" component={ChallengesStack} />
+		<Drawer.Navigator initialRouteName="Dashboard" drawerStyle={{ width: 230 }}>
+			<Drawer.Screen
+				name="Dashboard"
+				component={DashboardStack}
+				options={{
+					drawerIcon: () => {
+						return <MaterialIcons name="dashboard" size={25} style={{ color: theme.colors.primary }} />;
+					},
+				}}
+			/>
+			<Drawer.Screen
+				name="Measurements"
+				component={MeasurementsStack}
+				options={{
+					drawerIcon: () => {
+						return <MaterialIcons name="square-foot" size={25} style={{ color: theme.colors.primary }} />;
+					},
+				}}
+			/>
+			<Drawer.Screen
+				name="Challenges"
+				component={ChallengesStack}
+				options={{
+					drawerIcon: () => {
+						return <MaterialIcons name="insights" size={25} style={{ color: theme.colors.primary }} />;
+					},
+				}}
+			/>
 		</Drawer.Navigator>
 	);
 }
