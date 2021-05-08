ea9784d5bde31c2ddf2b85a9f545372a62adfa20
Yakub <jak.miskech@gmail.com>
2021-05-08 18:52:29 +0200

    Upgrade

--- src/store/staticData.ts
+++ src/store/staticData.ts
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import { v4 } from 'react-native-uuid';
+import uuid from 'react-native-uuid';
 
 import { RootStore } from './types';
 
@@ -22,49 +22,49 @@ export const measurement: RootStore['measurements'] = {
 	],
 	data: {
 		bodyweight: [
-			{ id: v4() as string, date: moment('2021-03-21', 'YYYY-MM-DD'), value: 88.4 },
-			{ id: v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 89.4 },
-			{ id: v4() as string, date: moment('2020-09-25', 'YYYY-MM-DD'), value: 89.4 },
-			{ id: v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 90.1 },
-			{ id: v4() as string, date: moment('2020-09-21', 'YYYY-MM-DD'), value: 89.5 },
-			{ id: v4() as string, date: moment('2020-09-16', 'YYYY-MM-DD'), value: 89.1 },
-			{ id: v4() as string, date: moment('2020-09-14', 'YYYY-MM-DD'), value: 88.6 },
-			{ id: v4() as string, date: moment('2020-04-21', 'YYYY-MM-DD'), value: 86.4 },
-			{ id: v4() as string, date: moment('2020-02-19', 'YYYY-MM-DD'), value: 91.5 },
-			{ id: v4() as string, date: moment('2020-02-17', 'YYYY-MM-DD'), value: 91 },
-			{ id: v4() as string, date: moment('2020-02-10', 'YYYY-MM-DD'), value: 90.9 },
-			{ id: v4() as string, date: moment('2020-02-05', 'YYYY-MM-DD'), value: 91 },
-			{ id: v4() as string, date: moment('2020-02-03', 'YYYY-MM-DD'), value: 91.4 },
-			{ id: v4() as string, date: moment('2020-01-29', 'YYYY-MM-DD'), value: 89.9 },
+			{ id: uuid.v4() as string, date: moment('2021-03-21', 'YYYY-MM-DD'), value: 88.4 },
+			{ id: uuid.v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 89.4 },
+			{ id: uuid.v4() as string, date: moment('2020-09-25', 'YYYY-MM-DD'), value: 89.4 },
+			{ id: uuid.v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 90.1 },
+			{ id: uuid.v4() as string, date: moment('2020-09-21', 'YYYY-MM-DD'), value: 89.5 },
+			{ id: uuid.v4() as string, date: moment('2020-09-16', 'YYYY-MM-DD'), value: 89.1 },
+			{ id: uuid.v4() as string, date: moment('2020-09-14', 'YYYY-MM-DD'), value: 88.6 },
+			{ id: uuid.v4() as string, date: moment('2020-04-21', 'YYYY-MM-DD'), value: 86.4 },
+			{ id: uuid.v4() as string, date: moment('2020-02-19', 'YYYY-MM-DD'), value: 91.5 },
+			{ id: uuid.v4() as string, date: moment('2020-02-17', 'YYYY-MM-DD'), value: 91 },
+			{ id: uuid.v4() as string, date: moment('2020-02-10', 'YYYY-MM-DD'), value: 90.9 },
+			{ id: uuid.v4() as string, date: moment('2020-02-05', 'YYYY-MM-DD'), value: 91 },
+			{ id: uuid.v4() as string, date: moment('2020-02-03', 'YYYY-MM-DD'), value: 91.4 },
+			{ id: uuid.v4() as string, date: moment('2020-01-29', 'YYYY-MM-DD'), value: 89.9 },
 		],
 		shoulders: [
-			{ id: v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 123 },
-			{ id: v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 121 },
+			{ id: uuid.v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 123 },
+			{ id: uuid.v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 121 },
 		],
 		chest: [
-			{ id: v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 112 },
-			{ id: v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 114 },
+			{ id: uuid.v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 112 },
+			{ id: uuid.v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 114 },
 		],
 		waist: [
-			{ id: v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 100.5 },
-			{ id: v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 104 },
+			{ id: uuid.v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 100.5 },
+			{ id: uuid.v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 104 },
 		],
 		hips: [
-			{ id: v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 110.5 },
-			{ id: v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 112 },
+			{ id: uuid.v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 110.5 },
+			{ id: uuid.v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 112 },
 		],
-		neck: [{ id: v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 39 }],
+		neck: [{ id: uuid.v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 39 }],
 		upper_arm_right: [],
 		upper_arm_left: [],
 		forearm_right: [],
 		forearm_left: [],
 		thigh_right: [
-			{ id: v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 58.5 },
-			{ id: v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 56 },
+			{ id: uuid.v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 58.5 },
+			{ id: uuid.v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 56 },
 		],
 		thigh_left: [
-			{ id: v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 59 },
-			{ id: v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 55.5 },
+			{ id: uuid.v4() as string, date: moment('2021-01-20', 'YYYY-MM-DD'), value: 59 },
+			{ id: uuid.v4() as string, date: moment('2020-09-22', 'YYYY-MM-DD'), value: 55.5 },
 		],
 		calf_right: [],
 		calf_left: [],
